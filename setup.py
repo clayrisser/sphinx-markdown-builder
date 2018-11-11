f9c088f44dd6fdce17dc41e64ec3f910e47b426c
Jam Risser <jam@codejam.ninja>
2018-11-11 01:09:43 -0600

    Updated version

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ with open(path.join(here, 'requirements.txt'), 'r', encoding='utf-8') as f:
 setup(
     name='sphinx-markdown-builder',
 
-    version='0.0.2',
+    version='0.0.3',
 
     description='sphinx markdown output',
 
