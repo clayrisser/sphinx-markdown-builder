f3d5f52cc86c19a0ddec357bbb209f71ccc5ad80
Jam Risser <jam@codejam.ninja>
2018-11-14 15:52:43 -0600

    Fixed title bug

--- sphinx_markdown_builder/markdown_writer.py
+++ sphinx_markdown_builder/markdown_writer.py
@@ -2,10 +2,10 @@ from .doctree2md import Translator, Writer
 
 class MarkdownTranslator(Translator):
     def visit_title(self, node):
-        self.add((self.section_level + 1) * '#' + ' ')
+        self.add((self.section_level) * '#' + ' ')
 
     def visit_subtitle(self, node):
-        self.add((self.section_level + 2) * '#' + ' ')
+        self.add((self.section_level + 1) * '#' + ' ')
 
 class MarkdownWriter(Writer):
     translator_class = MarkdownTranslator
