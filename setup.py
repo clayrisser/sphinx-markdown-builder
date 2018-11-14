54c37f4d80594f72ef315f60df447999c6fbb1d3
Jam Risser <jam@codejam.ninja>
2018-11-14 14:35:15 -0600

    Released v0.1.1

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ with open(path.join(here, 'requirements.txt'), 'r', encoding='utf-8') as f:
 setup(
     name='sphinx-markdown-builder',
 
-    version='0.1.0',
+    version='0.1.1',
 
     description='sphinx builder that outputs markdown files',
 
